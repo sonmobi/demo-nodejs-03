24094d23ea0f196172f04137ca7c3df37cb3f843
sonmobi <sonmobi@gmail.com>
2022-03-22 15:41:44 +0700

    a

--- routes/users.js
+++ routes/users.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/user', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/add', function (req, res){
+  res.render('users/add')
+})
+
+router.post('/add', function (req, res, next){
+     XuLyAdd(req.body)
+    res.send("OK") // gửi dữ liệu trả về client
+})
+function  XuLyAdd(data){
+  console.log(data)
+}
+
+
+router.get('/list', function (req,res){
+    res.json([{id:1,name:"ABC"},{id:2,name:"Xyz"}])
+})
+
 
 module.exports = router;
